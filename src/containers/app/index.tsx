68e9e5daddee7e7fc03b619cf49985e35fa339c0
Michal Brnak <michal.brnak@domo.com>
2020-03-17 20:59:30 -0600

    quick demo using refs to draw lines connecting them

---
+++ src/containers/app/index.tsx
@@ -0,0 +1,68 @@
+import React, { FunctionComponent, useEffect, useState, useRef } from 'react';
+
+import styles from './index.cssmodule.scss';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {
+  /* define props interface here */
+}
+
+export const App: FunctionComponent<Props> = (props) => {
+  // you can access the elements with itemsRef.current[n]
+  const [itemsRef, setItemsRef] = useState<Array<HTMLDivElement | null>>([]);
+  const [wasRendered, setWasRendered] = useState<boolean>(false);
+  useEffect(() => {
+    setItemsRef(itemsRef.slice(0, 10));
+    window.addEventListener('resize', function() {
+      setWasRendered(false);
+      setWasRendered(true);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log('itemsRef', itemsRef);
+    setWasRendered(true);
+  });
+
+  console.log(itemsRef);
+
+  const renderNodes = () => {
+    const nodes = [];
+    for (let i = 0; i < 10; i++) {
+      nodes.push(
+        <div
+          key={i}
+          className={styles.nodeContainer}
+          ref={(el) => (itemsRef[i] = el)}
+        >
+          <div className={styles.node}>{i}</div>
+        </div>,
+      );
+    }
+    return nodes;
+  };
+
+  const renderLines = () => {
+    return (
+      <svg className={styles.connector}>
+        {itemsRef.map((rect, i) =>
+          i < itemsRef.length - 1 ? (
+            <line
+              x1={itemsRef[i]?.getBoundingClientRect().x}
+              y1={itemsRef[i]?.getBoundingClientRect().y}
+              x2={itemsRef[i + 1]?.getBoundingClientRect().x}
+              y2={itemsRef[i + 1]?.getBoundingClientRect().y}
+              className={styles.line}
+            />
+          ) : null,
+        )}
+      </svg>
+    );
+  };
+  return (
+    <div className={styles.app}>
+      {renderNodes()}
+      {wasRendered ? renderLines() : null}
+    </div>
+  );
+};
